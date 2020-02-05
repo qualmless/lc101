6c9051a9ddcafd2b440fc03584ffea97aa4218b4
qualmless <terasmash@gmail.com>
2020-02-05 17:06:15 -0600

    moved HelloController to main from test lol

--- learningSpring/src/main/java/org/launchcode/learningSpring/controllers/HelloController.java
+++ learningSpring/src/main/java/org/launchcode/learningSpring/controllers/HelloController.java
@@ -1,5 +1,6 @@
 package org.launchcode.learningSpring.controllers;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
