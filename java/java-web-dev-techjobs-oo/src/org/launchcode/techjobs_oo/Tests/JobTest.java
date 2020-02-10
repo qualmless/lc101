c3ab3526b97d9c68080934a1a86d2077737a81bf
qualmless <terasmash@gmail.com>
2020-02-10 13:22:46 -0600

    learned @beforeEach for junit 5.4

---
+++ java/java-web-dev-techjobs-oo/src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,20 @@
+package org.launchcode.techjobs_oo.Tests;
+
+import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.jupiter.api.Before;
+import org.launchcode.techjobs_oo.Job;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JobTest {
+    Job test_job;
+
+    @Before
+    public void createTestJobObject() {}
+
+    @Test
+    void testSettingJobId() {
+        assertEquals(10,10,.001);
+    }
+}
