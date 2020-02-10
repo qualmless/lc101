6ae65c6dd9c0bfb7e4af8b0909070bb1ff693aeb
qualmless <terasmash@gmail.com>
2020-02-10 16:43:41 -0600

    testJobConstructorSetsAllFields completed

--- java/java-web-dev-techjobs-oo/src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ java/java-web-dev-techjobs-oo/src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -2,7 +2,7 @@ package org.launchcode.techjobs_oo.Tests;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.launchcode.techjobs_oo.Job;
+import org.launchcode.techjobs_oo.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -22,11 +22,26 @@ public class JobTest {
         assertEquals(10,10,.001);
     }
 
-//
-
     //TODO: testing ID values
     @Test
     public void testSettingJobId() {
         assertEquals(1, test_job2.getId()-test_job1.getId(), .001);
+    }
+
+    //TODO:Test the Full Constructor
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        Job test_job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertEquals("Product tester", test_job3.getName().toString());
+        assertEquals("ACME", test_job3.getEmployer().toString());
+        assertEquals("Desert", test_job3.getLocation().toString());
+        assertEquals("Quality control", test_job3.getPositionType().toString());
+        assertEquals("Persistence", test_job3.getCoreCompetency().toString());
+    }
+
+    //TODO: Test the equals Method
+    @Test
+    public void testJobsForEquality(){
+
     }
 }
