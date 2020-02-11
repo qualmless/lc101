1e1c3db62ff9de56cc3294c977197f04a76c1503
qualmless <terasmash@gmail.com>
2020-02-11 13:15:04 -0600

    renamed learningSpring to helloSpring for continuity with instructions

--- helloSpring/src/main/java/org/launchcode/learningSpring/HelloSpringApplication.java
+++ helloSpring/src/main/java/org/launchcode/learningSpring/HelloSpringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LearningSpringApplication {
+public class HelloSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearningSpringApplication.class, args);
+		SpringApplication.run(HelloSpringApplication.class, args);
 	}
 
 }
---
+++ java/java-web-dev-techjobs-oo/src/org/launchcode/techjobs_oo/JobField.java
@@ -0,0 +1,38 @@
+package org.launchcode.techjobs_oo;
+
+import java.util.Objects;
+
+public class JobField {
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (!(o instanceof Location)) return false;
+//        Location location = (Location) o;
+//        return getId() == location.getId();
+//    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+
+}
